082650c37c7dc5cbef19bdb54944aff759e31473
Douglas Howe <codemonk78@outlook.com>
2017-12-03 23:31:04 -0500

    setting up server

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,17 @@
-console.log('hi')
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
